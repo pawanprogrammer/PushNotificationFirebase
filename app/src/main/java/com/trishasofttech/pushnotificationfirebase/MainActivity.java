eda40e657f148660512b64652469008199368c97
Pawansharma <pawansharma548@gmail.com>
2020-10-20 21:24:59 +0530

    fourth

--- app/src/main/java/com/trishasofttech/pushnotificationfirebase/MainActivity.java
+++ app/src/main/java/com/trishasofttech/pushnotificationfirebase/MainActivity.java
@@ -1,29 +1,97 @@
 package com.trishasofttech.pushnotificationfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 Button btn_submit;
 EditText etname;
+ArrayList<String> arrayList1 = new ArrayList<String>();
+ArrayList<String> arrayList2 = new ArrayList<String>();
+
+    ArrayList<String> duplicateList = new ArrayList<String>();
+
+    ArrayList<String> uniqueList = new ArrayList<String>();
+
+    private static final String EMAIL = "email";
+    private static final String USER_POSTS = "user_posts";
+    private static final String AUTH_TYPE = "rerequest";
+
+    private CallbackManager mCallbackManager;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         etname = findViewById(R.id.etname);
         btn_submit = findViewById(R.id.btn_submit);
+
+        arrayList1.add("pawan sharma");
+        arrayList1.add( "rohan sharma");
+        arrayList1.add( "sohan sharma");
+        arrayList1.add( "mohan sharma");
+        arrayList1.add( "sagar sharma");
+        for (int i =0; i<arrayList1.size(); i++)
+        {
+            //Toast.makeText(this, arrayList1.get(i).toString(), Toast.LENGTH_SHORT).show();
+        }
+
+        arrayList2.add( "rohan sharma");
+        arrayList2.add( "sagar sharma");
+
+        // Variable names edited for readability
+        for (String item : arrayList1) {
+            if (arrayList2.contains(item)) {
+                duplicateList.add(item);
+            } else {
+                uniqueList.add(item);
+            }
+
+        }
+
+        for (int i=0; i<uniqueList.size();i++)
+        {
+            Toast.makeText(this, uniqueList.get(i).toString(), Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
         FirebaseApp.initializeApp(this);
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -60,5 +128,99 @@ EditText etname;
                 return true;
             }
         });
+
+
+
+
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mCallbackManager = CallbackManager.Factory.create();
+
+        LoginButton mLoginButton = findViewById(R.id.login_button);
+
+        // Set the initial permissions to request from the user while logging in
+        mLoginButton.setReadPermissions("email", "public_profile");
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //            Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                //Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                //Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
     }
+    // ...
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+        /*mLoginButton.setAuthType(AUTH_TYPE);
+        // Register a callback to respond to the user
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                setResult(RESULT_OK);
+                finish();
+            }
+            @Override
+            public void onCancel() {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+            @Override
+            public void onError(FacebookException e) {
+                // Handle exception
+            }
+        })*/;
+
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                                Intent intent = new Intent(MainActivity.this,
+                                        TestingActivity.class);
+                                startActivity(intent);
+                                finish();
+
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
